6e20531f8c99641b48baf6004c8ac7f4f0208fbc
mudassir <mudassirjabbar4@gmail.com>
2022-11-07 23:12:13 +0500

    second commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/screens/products_overview_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shop App',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange,
+      ),
+      home: ProductsOverviewScreen(),
+    );
+  }
+}
