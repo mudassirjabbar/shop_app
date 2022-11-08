69d4d4a1f2356df8e074734a059e73dbe2c95bd1
mudassir <mudassirjabbar4@gmail.com>
2022-11-08 06:33:34 +0500

    second commit

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../screens/product_detail_screen.dart';
+
 class ProductItem extends StatelessWidget {
   final String id;
   final String title;
@@ -35,9 +37,15 @@ class ProductItem extends StatelessWidget {
             icon: const Icon(Icons.shopping_cart),
           ),
         ),
-        child: Image.network(
-          imageUrl,
-          fit: BoxFit.cover,
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context)
+                .pushNamed(ProductDetailScreen.routeName, arguments: id);
+          },
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
         ),
       ),
     );
